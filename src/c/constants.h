77e23f9aa6483e298478ff7c5384a72c56b89cc9
Eduardo Fernandes <e.luis@icloud.com>
2017-08-13 21:20:15 +0100

    Remove useless constants

--- src/c/constants.h
+++ src/c/constants.h
@@ -6,16 +6,10 @@
 #define CONST_TOTAL_BLOCKS 25
 #define CONST_FONT_W       50
 #define CONST_FONT_W_HALF  25
-#define CONST_TILES_X     110
-#define CONST_TILES_Y      20
 
 #ifdef PBL_RECT
-#define CONST_ORIGIN_X 17
-#define CONST_ORIGIN_Y 15
 #define CONST_NUM_SLOTS 8
 #else
-#define CONST_ORIGIN_X 35
-#define CONST_ORIGIN_Y 22
 #define CONST_NUM_SLOTS 18
 #endif
 
